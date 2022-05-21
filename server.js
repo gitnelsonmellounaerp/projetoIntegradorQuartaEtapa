a7593e009852912c43c7d8ce4260019e4016852b
Nelson Mello <nelson.mello@sou.unaerp.edu.br>
2022-05-21 18:32:02 -0300

    teste

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.set("view engine", "ejs"); // faz o express renderizar os arquivos .ejs (precisa instalar o modulo ejs)
 app.use(express.static("public")); // faz o express ler arquivos estaticos na pasta public
+
 const bootstrap = require('bootstrap'); // ler o módulo do bootstrap
 
 
